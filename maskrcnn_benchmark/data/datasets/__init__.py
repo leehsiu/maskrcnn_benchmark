ad7d5ad352f08e290521769cc27b188ffd943a15
leehsiu <lixiulive@hotmail.com>
2019-04-16 17:44:02 -0400

    update dataset folder

--- maskrcnn_benchmark/data/datasets/__init__.py
+++ maskrcnn_benchmark/data/datasets/__init__.py
@@ -1,6 +1,7 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
 from .coco import COCODataset
 from .voc import PascalVOCDataset
+from .densepose_coco import DensePoseCOCODataset
 from .concat_dataset import ConcatDataset
 
-__all__ = ["COCODataset", "ConcatDataset", "PascalVOCDataset"]
+__all__ = ["COCODataset", "DensePoseCOCODataset","ConcatDataset", "PascalVOCDataset"]
