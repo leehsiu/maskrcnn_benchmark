f22f8f3cad11b56a76be76bb24a96cc14fd0f49a
leehsiu <lixiulive@hotmail.com>
2019-04-16 17:26:30 -0400

    local

--- maskrcnn_benchmark/config/paths_catalog.py
+++ maskrcnn_benchmark/config/paths_catalog.py
@@ -5,7 +5,7 @@ import os
 
 
 class DatasetCatalog(object):
-    DATA_DIR = "datasets"
+    DATA_DIR = os.path.join(os.path.dirname(__file__),'../datasets')
     DATASETS = {
         "coco_2017_train": {
             "img_dir": "coco/train2017",
